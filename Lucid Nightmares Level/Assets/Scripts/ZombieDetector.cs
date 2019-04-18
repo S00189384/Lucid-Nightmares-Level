28cb892c3603df8b05f16b384aab71335f3e631c
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2019-04-18 22:51:23 +0100

    v32
    door bug fixed

--- Lucid Nightmares Level/Assets/Scripts/ZombieDetector.cs
+++ Lucid Nightmares Level/Assets/Scripts/ZombieDetector.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class ZombieDetector : MonoBehaviour
 {
     public bool ZombiesInArea;
+    public int numZombies;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Zombie" || collision.gameObject.tag == "SpawnPoint")
+        if (collision.gameObject.tag == "Zombie")
+        {
+            numZombies++;
+            ZombiesInArea = true;
+        }
+        if(collision.gameObject.tag == "SpawnPoint")
         {
             ZombiesInArea = true;
         }
@@ -18,7 +24,11 @@ public class ZombieDetector : MonoBehaviour
     {
         if (collision.gameObject.tag == "Zombie" || collision.gameObject.tag == "SpawnPoint")
         {
-            ZombiesInArea = false;
+            numZombies--;
+            if(numZombies <= 0)
+            {
+                ZombiesInArea = false;
+            }
         }
     }
 }
