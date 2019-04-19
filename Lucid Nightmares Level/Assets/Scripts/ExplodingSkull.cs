15e531b489993c7e44387ed9dcd1b63b5ac9a24b
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2019-04-19 19:48:33 +0100

    v34
    Start of exploding skull. Third ghost goes into tunnel

---
+++ Lucid Nightmares Level/Assets/Scripts/ExplodingSkull.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingSkull : MonoBehaviour
+{
+    public GameObject player;
+    Rigidbody2D body;
+    public float rotation = 2;
+    public float distanceToPlayer;
+    public float timer;
+    public float explodeTime = 2;
+    public bool CanExplode = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        body = GetComponent<Rigidbody2D>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer < 5)
+        {
+            CanExplode = true;
+        }
+        if(CanExplode)
+        {
+            rotation += 0.10f;
+            transform.Rotate(0, 0, rotation);
+            timer += Time.deltaTime;
+
+            if(timer >= explodeTime)
+            {
+
+
+
+            }
+        }
+	}
+}
---
+++ Lucid Nightmares Level/Assets/Scripts/ThirdGhostController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdGhostController : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    public Transform positionToMove;
+    public GameObject player;
+    Rigidbody2D body;
+    public float moveSpeed = 5;
+    public float distanceToPlayer;
+    public bool CanMove = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer <= 2)
+        {
+            CanMove = true;               
+        }
+
+        if (CanMove == true)
+        {
+            sprite.flipX = true;
+            body.MovePosition(Vector2.MoveTowards(transform.position, positionToMove.transform.position, moveSpeed * Time.deltaTime));          
+        }
+
+        if (Vector2.Distance(transform.position, positionToMove.transform.position) <= 0.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
