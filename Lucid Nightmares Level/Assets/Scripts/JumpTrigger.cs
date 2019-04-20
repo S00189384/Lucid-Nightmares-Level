373f5fb6022cbd92329e99d5306d806e36de6109
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2019-04-20 17:52:28 +0100

    v37
    Started next part of level and jump trigger

---
+++ Lucid Nightmares Level/Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    Rigidbody2D body;
+    GameObject player;
+
+    public float distanceToPlayer;
+    public Vector2 jumpForce = new Vector2(0, 8);
+    public float rotation;
+    public float maxRotation = 10;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer <= 5)
+        {
+            if (rotation > maxRotation)
+            {
+                rotation = maxRotation;
+            }
+            rotation += 0.5f;
+            transform.Rotate(0, 0, rotation);
+
+        }
+        else
+        {
+            rotation = 0;
+            transform.Rotate(0, 0, rotation);
+        }
+		
+	}
+}
