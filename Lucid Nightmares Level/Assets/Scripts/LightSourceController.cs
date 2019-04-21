ae97b5d2e76ef3ac028108fec14fa8ea851cc991
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2019-04-21 22:31:35 +0100

    v40
    Invis room kinda done

--- Lucid Nightmares Level/Assets/Scripts/LightSourceController.cs
+++ Lucid Nightmares Level/Assets/Scripts/LightSourceController.cs
@@ -8,22 +8,55 @@ public class LightSourceController : MonoBehaviour
     Rigidbody2D body;
     public float elapsedTime;
     public float destroyTime = 7;
+    public float visibleRadius = 5;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    void Update ()
+    void Update()
     {
         elapsedTime += Time.deltaTime;
         if (elapsedTime >= destroyTime)
             Destroy(gameObject);
 
-	}
+        InvisibleTileShowHide();
+    }
+
+
+    // Method to show / hide invisible tiles based on proximity to the light source. Just before the destroy time it ensures that the sprites of  
+    // any tiles affected by the light source are turned off before the lights source is destroyed.
+    public void InvisibleTileShowHide()
+    {
+        Collider2D[] objectsInRadius = Physics2D.OverlapCircleAll(transform.position, visibleRadius);
+        for (int i = 0; i < objectsInRadius.Length; i++)
+        {
+            // If the invis tiles are tagged correctly and within range, ignore collision so the light source can pass through and enable sprite
+            // as long as the light source has not been destroyed.
+            if (objectsInRadius[i].gameObject.tag == "InvisTiles" || objectsInRadius[i].gameObject.tag == "Trapdoor"/*Vector2.Distance(transform.position, objectsInRadius[i].transform.position) <= visibleRadius*/)
+            {
+                Physics2D.IgnoreCollision(GetComponent<Collider2D>(), objectsInRadius[i].GetComponent<Collider2D>());
+                if(elapsedTime < destroyTime && Vector2.Distance(transform.position, objectsInRadius[i].transform.position) <= visibleRadius)
+                {
+                    objectsInRadius[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                if(elapsedTime >= destroyTime || Vector2.Distance(transform.position, objectsInRadius[i].transform.position) > visibleRadius)
+                {
+                    objectsInRadius[i].GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+
+            // If the invis tiles are out of range - disable sprite
+            //else if (objectsInRadius[i].gameObject.tag == "InvisTiles" && Vector2.Distance(transform.position, objectsInRadius[i].transform.position) > visibleRadius)
+            //{
+            //    objectsInRadius[i].GetComponent<SpriteRenderer>().enabled = false;
+            //}
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    { 
         if (collision.gameObject)
         {
             //Hits enemy, it stops but doesn't block enemy.
@@ -32,3 +65,4 @@ public class LightSourceController : MonoBehaviour
         }
     }
 }
+
